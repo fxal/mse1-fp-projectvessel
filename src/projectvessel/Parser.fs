a8b48fc4fe29a295d4f5b60d1a9e3fff4b817eaa
Sigrid <sigrid.huemer@posteo.at>
2021-01-16 18:25:45 +0100

    visit different rooms

--- src/projectvessel/Parser.fs
+++ src/projectvessel/Parser.fs
@@ -15,10 +15,21 @@ let (|ConfirmEradication|SelfDestruct|Visit|LogOff|Help|ParseFailed|) (input: st
 
     let parts = input.Split(' ') |> List.ofArray
 
+    let tryParseAssessmentRoom (arg: string) valueConstructor =
+        let (worked, arg') =
+            match arg with
+            | "Tech" -> (true, Domain.TechAss)
+            | "Perfection" -> (true, Domain.PerfectionAss)
+            | "Thread" -> (true, Domain.ThreadAss)
+            | "Damage" -> (true, Domain.DamageAss)
+            | _ -> (false, Domain.Hyperspace)
+
+        if worked then valueConstructor arg' else ParseFailed
+
     match parts with
     | [ verb ] when safeEquals verb (nameof Domain.EradicatePlanet) -> ConfirmEradication
     | [ verb ] when safeEquals verb (nameof Domain.SelfDestruct) -> SelfDestruct
     | [ verb ] when safeEquals verb HelpLabel -> Help
-    | [ verb; arg ] when safeEquals verb (nameof Domain.Visit) -> Visit Domain.TechAss // TODO: visit room based on string
+    | [ verb; arg ] when safeEquals verb (nameof Domain.Visit) -> tryParseAssessmentRoom arg (fun value -> Visit value)
     | [ verb ] when safeEquals verb (nameof Domain.LogOff) -> LogOff
     | _ -> ParseFailed
